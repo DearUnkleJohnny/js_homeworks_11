d94b8026dac2e6923b05f4f5b3e2a19c5121e07f
DearUnkleJohnny <shuma.lov@mail.ru>
2023-03-14 22:29:05 +0300

    Homeworks11

---
+++ script.js
@@ -0,0 +1,37 @@
+const productsData = [{
+    img: "./img/content_bottom_1.jpg",
+    linkText: 'Куртка "QWETRY"',
+    textProduct: 'Осенняя куртка из сентипона, дёшево и беспонтово!',
+    price: '$25.00'
+},
+{
+    img: "./img/content_bottom_2.png",
+    linkText: 'Костюм "IVIVI"',
+    textProduct: 'Хз что за костюм, не разбераюсь в женской одежде',
+    price: '$58.00'
+},
+{
+    img: "./img/content_bottom_3.png",
+    linkText: 'Худи "TOY"',
+    textProduct: 'Худи на любителя.',
+    price: '$44.50'
+},
+{
+    img: "./img/content_bottom_4.png",
+    linkText: 'Брюки "CAPSULE"',
+    textProduct: 'Жёлтые, модные брюки - живи ярко!',
+    price: '$35.00'
+},
+{
+    img: "./img/content_bottom_5.png",
+    linkText: 'Пиджак "BLUE JACKET"',
+    textProduct: 'Пиджак голубой, больше добавить нечего.',
+    price: '$77.30'
+},
+{
+    img: "./img/content_bottom_6.png",
+    linkText: 'Полосатая рубашка "PRISON"',
+    textProduct: 'Небо в клеточку, друзья в полосочку.',
+    price: '$105.00'
+}
+]
